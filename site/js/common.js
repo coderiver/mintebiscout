3eeb01b12ebbc708d831976986ed78fd4998b318
akella <akella.a@gmail.com>
2015-11-17 12:02:26 +0200

    email validation

--- site/js/common.js
+++ site/js/common.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
-	
-	if($(window).width()<751){
+	function validateEmail(email) {
+	    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	    return re.test(email);
+	}
+	var error = 0;
+	$('.js-submit').click(function(event) {
+		event.preventDefault(); 
+		$('.js-form input,.js-form textarea').removeClass('is-error');
+		error = 0;
+		if($('.input-name').val()=='') {
+				$('.input-name').addClass('is-error');error = 1;
+			}
+		if($('.input-company').val()=='') {
+				$('.input-company').addClass('is-error'); error = 1;
+			}
+		if($('.input-request').val()=='') {
+				$('.input-request').addClass('is-error');error = 1;
+			}
+		ema = $('.input-mail').val();
+		if(!validateEmail(ema)) {
+				$('.input-mail').addClass('is-error');error = 1;
+			}
+		
+		if(error!=1){
+			// here goes submit
+			$.ajax({
+				type: "POST",
+				url: "/send.php",
+				data: $(".js-form").serialize(), // serializes the form's elements.
+				success: function(data) {
+					$('.js-form').addClass('is-success');
+					console.log(data);
+					setTimeout(function(){$('.form').removeClass("is-success");}, 5000);
+				}
+			});
+		}
+		return false;
+	});
 
+	if($(window).width()<751){
 		$('.js-experts').slick({
 			slidesToShow: 1,
 		  	slidesToScroll: 1,
@@ -14,5 +51,4 @@ $(document).ready(function() {
 		})
 	}
 
-	console.log($('body').html());
 });
